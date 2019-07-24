175e18b08ba2a45a5b780932f318a676ff4d9231
Hawking <hawkingxp@gmail.com>
2019-07-24 14:29:29 +0800

    firstcommit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // mongoURI: 'mongodb://test:test1234@ds131942.mlab.com:31942/node-vue-element',
+  mongoURI: 'mongodb://root:Aa123456@ip:27017/test?authSource=admin',
+  secretOrKey: 'secret'
+};
